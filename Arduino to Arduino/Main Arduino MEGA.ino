017d59f7345f842858b3629f98dbaa4f1899c91b
Oleg Nikitashin <nikitashin.ov@gmail.com>
2024-08-08 12:04:31 -0700

    output pins updated

--- Arduino to Arduino/Main Arduino MEGA.ino
+++ Arduino to Arduino/Main Arduino MEGA.ino
@@ -1,4 +1,4 @@
-
+//RELAYS ON BOARD A and C
 #define RELAY_A_R1 46 //Let's pretend it is Board A and name for relay = RELAY_A_R1 where A-name of the board and R1-Red LED number 1
 #define RELAY_A_G1 48 //Let's pretend it is Board A and name for relay = RELAY_A_G1 where A-name of the board and G1-Green LED number 1
 #define RELAY_A_R2 50
@@ -8,42 +8,40 @@
 #define RELAY_C_G2 51
 #define RELAY_C_R2 53
 
+//RELAYS ON BOARD D
+#define RELAY_D_G1 45
+#define RELAY_D_R1 43
+#define RELAY_D_G2 41
+#define RELAY_D_R2 39
+
+
+//INPUT PINS
+#define RFID1_D_G 2 //RFID1_D_G means that rfid reader number 1 is located on board D and it manages green LED
+#define RFID1_D_R 3 //RFID1_D_R means that rfid reader number 1 is located on board D and it manages red LED
+#define RFID2_D_G 4
+#define RFID2_D_R 5
+#define RFID3_D_G 6
+#define RFID3_D_R 7
+
 
 
 void setup() 
 {
   //start serial connection
   Serial.begin(9600);
-  //configure pin 2 as an input and enable the internal pull-up resistor
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
-  pinMode(5, INPUT_PULLUP);
-  pinMode(6, INPUT_PULLUP);
-  pinMode(7, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);
-  pinMode(10, INPUT_PULLUP);
-  pinMode(11, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
-  pinMode(13, INPUT_PULLUP);
-  pinMode(22, INPUT_PULLUP);
-  pinMode(24, INPUT_PULLUP);
-
-  
-  pinMode(53, OUTPUT);
-  pinMode(51, OUTPUT);
-  pinMode(49, OUTPUT);
-  pinMode(47, OUTPUT);
-  pinMode(45, OUTPUT);
-  pinMode(43, OUTPUT);
-  pinMode(41, OUTPUT);
-  pinMode(39, OUTPUT);
-  pinMode(46, OUTPUT);
-  pinMode(48, OUTPUT);
-  pinMode(50, OUTPUT);
-  pinMode(52, OUTPUT);
-
+  //configure pins as an input and enable the internal pull-up resistor
+  pinMode(RFID1_D_G, INPUT_PULLUP);
+  pinMode(RFID1_D_R, INPUT_PULLUP);
+  pinMode(RFID2_D_G, INPUT_PULLUP);
+  pinMode(RFID2_D_R, INPUT_PULLUP);
+  pinMode(RFID3_D_G, INPUT_PULLUP);
+  pinMode(RFID3_D_R, INPUT_PULLUP);
+   
+  pinMode(RELAY_D_G1, OUTPUT);
+  pinMode(RELAY_D_R1, OUTPUT);
+  pinMode(RELAY_D_G2, OUTPUT);
+  pinMode(RELAY_D_R2, OUTPUT);
+ 
   // digitalWrite(2, LOW);
   // digitalWrite(3, LOW);
   // digitalWrite(4, LOW);
@@ -56,14 +54,6 @@ void setup()
 }
 
 void loop() {
-  //read the pushbutton value into a variable
-  bool RFID_E&F_1_G = digitalRead(2);
-  bool RFID_E&F_1_R = digitalRead(3);
-  bool RFID_E&F_2_G = digitalRead(4);
-  bool RFID_E&F_2_R = digitalRead(5); 
-  bool RFID_E&F_3_G = digitalRead(6);
-  bool RFID_E&F_3_R = digitalRead(7); 
-
   //print out the value of the pushbutton
 
   // Keep in mind the pull-up means the pushbutton's logic is inverted. It goes
@@ -71,7 +61,7 @@ void loop() {
   // button's pressed, and off when it's not:
 
   //GREEN
-  if (RFID_A_1 == 1) 
+  if (RFID1_D_G == 1) 
   {
     Serial.print("RFID_A_1 pin 53: ");
     Serial.println(RFID_A_1);
