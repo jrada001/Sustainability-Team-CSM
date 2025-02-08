75dbaaf73497d767000f488cc514ebab4797f846
jrada001 <jrada0014554@gmail.com>
2025-02-08 11:25:03 -0800

    Update and rename Nano0.ino to Nano_A.ino

--- TX-RXv2.0/Nano_A.ino
+++ TX-RXv2.0/Nano_A.ino
@@ -1,14 +1,16 @@
+// NANO A
+
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define SS_PIN_1 2
 #define RST_PIN_1 9
+#define SS_PIN_1 2
 #define SS_PIN_2 3
-#define SS_PIN_3 4
+
 
 MFRC522 rfid1(SS_PIN_1, RST_PIN_1);
 MFRC522 rfid2(SS_PIN_2, RST_PIN_1);
-MFRC522 rfid3(SS_PIN_3, RST_PIN_1);
+
 
 String readUID(MFRC522 &rfid) {
   String uidString = "";
@@ -23,16 +25,16 @@ void setup() {
   SPI.begin();
   rfid1.PCD_Init();
   rfid2.PCD_Init();
-  rfid3.PCD_Init();
+
 }
 
 void loop() {
   if (rfid1.PICC_IsNewCardPresent() && rfid1.PICC_ReadCardSerial()) {
     String uid = readUID(rfid1);
     if (uid == "c3c55510" || uid == "d3325d10" || uid == "d3f74310") {
-      Serial.println("1_RED");
+      Serial.println("A1_RED");
     } else if (uid == "435e4410" || uid == "b3604510" || uid == "73606210") {
-      Serial.println("1_GREEN");
+      Serial.println("A1_GREEN");
     }
     rfid1.PICC_HaltA();
   }
@@ -40,20 +42,12 @@ void loop() {
   if (rfid2.PICC_IsNewCardPresent() && rfid2.PICC_ReadCardSerial()) {
     String uid = readUID(rfid2);
     if (uid == "c3c55510" || uid == "d3325d10" || uid == "d3f74310") {
-      Serial.println("2_RED");
+      Serial.println("A2_RED");
     } else if (uid == "435e4410" || uid == "b3604510" || uid == "73606210") {
-      Serial.println("2_GREEN");
+      Serial.println("A2_GREEN");
     }
     rfid2.PICC_HaltA();
   }
 
-  if (rfid3.PICC_IsNewCardPresent() && rfid3.PICC_ReadCardSerial()) {
-    String uid = readUID(rfid3);
-    if (uid == "c3c55510" || uid == "d3325d10" || uid == "d3f74310") {
-      Serial.println("3_RED");
-    } else if (uid == "435e4410" || uid == "b3604510" || uid == "73606210") {
-      Serial.println("3_GREEN");
-    }
-    rfid3.PICC_HaltA();
-  }
+
 }
